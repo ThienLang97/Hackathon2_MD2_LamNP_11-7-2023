a3b39eae3230efc163d73a4847ae85d07ed96713
ThienLang97 <lampr494@gmail.com>
2023-11-07 17:22:36 +0700

    add

---
+++ Hackathon2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Spending from './components/Spending'
+import './components/Spending.css'
+export default function App() {
+  return (
+    <>
+      <Spending></Spending>
+    </>
+  )
+}
---
+++ Hackathon2/src/components/Spending.jsx
@@ -0,0 +1,247 @@
+import React, { useState } from 'react'
+import './Spending.css'
+
+export default function Spending() {
+    const [totalMoney, setTotalMoney] = useState(12000000000)
+    const [cartItems, setCartItems] = useState([]);
+    const [products, setProducts] = useState([
+        {
+            id: 1,
+            title: "Cola",
+            price: 6000,
+            count: 0,
+            image:
+                "https://ayb.akinoncdn.com/products/2019/01/18/3544/5aa1ee14-1c83-4213-a62b-773c4785e187_size780x780_quality60_cropCenter.jpg",
+        },
+        {
+            id: 3,
+            title: "Sprite",
+            price: 5600,
+            count: 0,
+            image: "https://images.ofix.com/product-image/s99509_2.jpg",
+        },
+        {
+            id: 4,
+            title: "Ayran",
+            price: 80000,
+            count: 0,
+            image:
+                "https://konyamevlana.com/florya/wp-content/uploads/2020/12/sutas-ayran-285ml.jpg",
+        },
+        {
+            id: 5,
+            title: "Salgam",
+            price: 400000,
+            count: 0,
+            image: "http://esenlik.com.tr//images/prod/5704.jpg",
+        },
+        {
+            id: 6,
+            title: "Cay",
+            price: 300000,
+            count: 0,
+            image:
+                "https://evidea.akinoncdn.com/products/2021/08/05/62421/e5bda9ce-91bf-453a-96e1-acea983efb2a.jpg",
+        },
+        {
+            id: 7,
+            title: "Kahve",
+            price: 10,
+            count: 0,
+            image:
+                "https://media.istockphoto.com/photos/turkish-coffee-foamy-picture-id510413268?k=20&m=510413268&s=612x612&w=0&h=llBhDOUbNHaQXc-ch7UQ_OIWkmJAJUzaf6oZavKUrmQ=",
+        },
+        {
+            id: 8,
+            title: "Soda",
+            price: 4,
+            count: 0,
+            image: "http://esenlik.com.tr//images/prod/2928.jpg",
+        },
+        {
+            id: 9,
+            title: "Bisiklet",
+            price: 1000,
+            count: 0,
+            image:
+                "https://productimages.hepsiburada.net/s/37/375/10567819821106.jpg",
+        },
+
+        {
+            id: 11,
+            title: "Bugatti Chiron",
+            price: 2500000,
+            count: 0,
+            image:
+                "https://productimages.hepsiburada.net/s/34/375/10460429320242.jpg",
+        },
+        {
+            id: 12,
+            title: "Iskender",
+            price: 50,
+            count: 0,
+            image:
+                "https://img3.aksam.com.tr/imgsdisk/2020/10/31/t25_en-kolay-iskender-sosu-ta-744.jpg",
+        },
+        {
+            id: 13,
+            title: "Lahmacun",
+            price: 12,
+            count: 0,
+            image:
+                "https://cdn.yemek.com/mnresize/940/940/uploads/2020/04/lahmacun-yeni-one-cikan.jpg",
+        },
+        {
+            id: 14,
+            title: "Malikane",
+            price: 2500000000,
+            count: 0,
+            image:
+                "https://foto.haberler.com/haber/2020/12/07/israil-de-4-odali-malikane-250-milyon-dolarda-13784975_amp.jpg",
+        },
+        {
+            id: 15,
+            title: "Helicopter",
+            price: 28750000,
+            count: 0,
+            image:
+                "https://image.elitema.com.tr/db_images/224/4/11/arimg1138-8---bell-429-exter%C4%B1or.jpg",
+        },
+        {
+            id: 16,
+            title: "Luxury Yatch",
+            price: 17500000,
+            count: 0,
+            image: "https://d.neoldu.com/news/57966.jpg",
+        },
+        {
+            id: 17,
+            title: "Limosine",
+            price: 1000000,
+            count: 0,
+            image:
+                "https://img.paratic.com/dosya/2015/03/dunyanin-en-pahali-limuzinleri-1024x683.jpg",
+        },
+        {
+            id: 18,
+            title: "Ada",
+            price: 125000000,
+            count: 0,
+            image: "https://icdn.ensonhaber.com/resimler/galeri/1_11195.jpg",
+        },
+        {
+            id: 22,
+            title: "Stadium",
+            price: 2500000,
+            count: 0,
+            image:
+                "https://cdnuploads.aa.com.tr/uploads/Contents/2020/06/01/thumbs_b_c_dc24a18cc233bd960f410911f5d39bf2.jpg",
+        },
+        {
+            id: 23,
+            title: "Bitcoin",
+            price: 60000,
+            count: 0,
+            image:
+                "https://www.cumhuriyet.com.tr/Archive/2021/9/27/1872247/kapak_141123.jpg",
+        },
+
+    ]);
+    const [counts, setCounts] = useState(Array(products.length).fill(0));
+    //
+    const increaseCount = (index, price) => {
+        const newCounts = [...counts];
+        newCounts[index] += 1;
+        setCounts(newCounts);
+        setTotalMoney(totalMoney - price);
+        //
+        const newCartItem = {
+            image: products[index].image,
+            product: products[index],
+            price: products[index].price,
+            quantity: newCounts[index]
+        };
+        const existingItemIndex = cartItems.findIndex(item => item.product.id === newCartItem.product.id);
+
+        if (existingItemIndex !== -1) {
+            const updatedCartItems = [...cartItems];
+            updatedCartItems[existingItemIndex].quantity = newCartItem.quantity;
+            setCartItems(updatedCartItems);
+        } else {
+            setCartItems([...cartItems, newCartItem]);
+        }
+
+    };
+    console.log(cartItems, "123123");
+
+    const decreaseCount = (index,price) => {
+        let zzz
+        const updatedCartItems1 = cartItems.map((item) => {
+            zzz = item.quantity
+            if (item.quantity > 0) {
+                return {
+                    ...item,
+                    quantity: item.quantity - 1,
+                };
+                
+            }
+            return item;
+            
+        });
+        const newCounts = [...counts];
+        newCounts[index] -= 1;
+        setCounts(newCounts);
+        setCartItems(updatedCartItems1);
+        setTotalMoney(totalMoney + price);
+        
+    };
+    const clearCart = (index)=>{
+        setCartItems([]);
+        const newCounts = [...counts];
+        newCounts[index] = 0;
+        setCounts(newCounts);
+    }
+    return (
+        <>
+            <div className='header' style={{ position: 'sticky', top: 0 }}>
+                <h2>To Spend <span>{totalMoney}$ </span> You have a lot of money</h2>
+            </div>
+            <div className='productsList'>
+                {products.map((product, index) => (
+                    <div className='product' key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <p>Tiền: {product.price}</p>
+                        <button onClick={() => increaseCount(index, product.price)}>Thêm</button>
+                        <p>Số lượng: {counts[index]}</p>
+                        <button onClick={() => decreaseCount(index, product.price)}>Bỏ</button>
+                    </div>
+                ))}
+            </div>
+            <div className='cart'>
+                <h3>Giỏ hàng</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tên sản phẩm</th>
+                            <th>Số lượng</th>
+                            <th>Giá</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {cartItems.map((item, index) => (
+                            <tr key={index}>
+                                <td>
+                                    <img src={item.image} alt={item.product.title} />
+                                    <p>{item.product.title}</p>
+                                </td>
+                                <td>{item.quantity}</td>
+                                <td>{item.price}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                    <button onClick={clearCart}>Xóa hết mặt hàng</button>
+            </div>
+        </>
+    )
+}
